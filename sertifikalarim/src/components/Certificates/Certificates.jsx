a9b0a4f8d33b4d849c95dfe4d40db4d23d37b019
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-11-24 12:35:37 +0300

    notlar

--- sertifikalarim/src/components/Certificates/Certificates.jsx
+++ sertifikalarim/src/components/Certificates/Certificates.jsx
@@ -12,11 +12,9 @@ console.log(cer);
         <h4  className='title-certifica'>{title}</h4>
         <h5 className='description'>{description}</h5> 
     </div>
-
-  
-
-
-  )
+// Notlar
+// serfikalarımımı yaparken diğer porje ile birleştirmek istiyorum 
+// 2 react eğitimi bitti ve projeler bazlı çalışacağım
 }
 
 export default Certificates
