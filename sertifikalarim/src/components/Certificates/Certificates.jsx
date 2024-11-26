fdb5b4216d708954c505849312c5df3537ca7d1f
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-11-26 17:08:01 +0300

    düzenleme

--- sertifikalarim/src/components/Certificates/Certificates.jsx
+++ sertifikalarim/src/components/Certificates/Certificates.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 
-function Certificates({cer}) {
-const { id, title, description,image} = cer;    
-
-console.log(cer);
+function Certificates({ cer, deneme }) {
+  const { id, title, description, image } = cer;
+  const deneme = true;
 
   return (
-    
+
     <div className='certificates'>
-        <img className='images' src={image}></img>
-        <h4  className='title-certifica'>{title}</h4>
-        <h5 className='description'>{description}</h5> 
+      <img className='images' src={image}></img>
+      <h4 className='title-certifica'>{title}</h4>
+      <h5 className='description'>{description}</h5>
+      <h2>{deneme ? "Evet" : "Hayır"}</h2>
+      <h2>{deneme && "Evet"}</h2>
     </div>
-// Notlar
-// serfikalarımımı yaparken diğer porje ile birleştirmek istiyorum 
-// 2 react eğitimi bitti ve projeler bazlı çalışacağım
+  )
+
+  //todo  Notlar
+  //! serfikalarımımı yaparken diğer porje ile birleştirmek istiyorum 
+  //? 2 react eğitimi bitti ve projeler bazlı çalışacağım
 }
 
 export default Certificates
